d82df3e53a800eeab53fbc5981359a45103fc131
Samuel <alphaelena@gmail.com>
2021-04-22 07:53:20 -0400

    extracted projectid from path.

--- section-3-content/pages/portfolio/[projectid].js
+++ section-3-content/pages/portfolio/[projectid].js
@@ -1,4 +1,8 @@
+import {useRouter} from 'next/router'
 const PortfolioProjectPage = () => {
+    const router = useRouter()
+    console.log("router.pathname",router.pathname)
+    console.log("router.query",router.query)
     return <div>
         <h1> Le Portfolio Project Page </h1>
     </div>
